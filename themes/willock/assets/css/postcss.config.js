423fd9155e1e3036b06a95efc7b3661c0dfa5631
ewen_ <ewenhenderson@gmail.com>
2021-02-28 16:46:50 +0000

    fix

--- themes/willock/assets/css/postcss.config.js
+++ themes/willock/assets/css/postcss.config.js
@@ -1,5 +1,17 @@
 const themeDir = __dirname + '/../../';
 
+const purgecss = require('@fullhuman/postcss-purgecss')({
+    // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
+    content: [
+        './hugo_stats.json',
+        themeDir + '/hugo_stats.json'
+    ],
+    defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+    }
+})
+
 module.exports = {    
     plugins: [   
         require('postcss-import')({
@@ -9,5 +21,6 @@ module.exports = {
         require('autoprefixer')({
             path: [themeDir]
         }),
+        ...(process.env.HUGO_ENVIRONMENT === 'production' ? [purgecss] : [])
     ]
 }
