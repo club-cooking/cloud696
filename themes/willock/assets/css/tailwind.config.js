c2bf8842edf22e8e0802f3b54e89ac5377c78558
ewen_ <ewenhenderson@gmail.com>
2021-03-11 18:44:17 +0000

    add tailwind typography

--- themes/willock/assets/css/tailwind.config.js
+++ themes/willock/assets/css/tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   theme: {
-    extend: {}
+    extend: {
+    	typography: (theme) => ({
+    		DEFAULT: {
+    			css: {
+    				color: theme('colors.white'),
+    				h1: {
+          				color: theme('colors.white'),
+          			},
+          			h2: {
+          				color: theme('colors.white'),
+          			},
+          			a: {
+          				color: theme('colors.white'),
+                  'text-decoration': 'none'
+          			},
+          			'figure figcaption': {
+          				color: theme('colors.white'),
+          			},
+    			}
+    		}
+    	})
+    }
   },
   variants: {},
-  plugins: []
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
 }
